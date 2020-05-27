1413169a872e9dd02c6161740e78586628f96938
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-27 18:07:12 +0900

    연습문제 제시

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
@@ -10,6 +10,17 @@ import kr.co.tjoeun.apipractice_20200527.databinding.ActivitySignUpBinding;
 public class SignUpActivity extends BaseActivity {
 
     ActivitySignUpBinding binding;
+//    응용문제.
+//    비번을 타이핑할 때마다 길이 검사
+//     => 0글자 : 비밀번호를 입력해주세요.
+//     => 8글자 미만 : 비밀번호가 너무 짧습니다.
+//     => 그 이상 : 사용핻 좋은 비밀번호 입니다.
+
+//    비밀 확인도 타이핑할 때마다 검사.
+//     => 0글자 : 비밀번호 확인을 입력해주세요.
+//     => 비번과 같다 : 비밀번호 재입력이  확인 되었습니다.
+//     => 다르다 : 비밀번호가 서로 다릅니다.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
