eb62569efeb6d4470120e813d8167f63980c920f
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-27 16:52:04 +0900

    서버의 응답을 액티비티에서 처리

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/utils/ServerUtil.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/utils/ServerUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.Log;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -24,7 +25,7 @@ public class ServerUtil {
         void onResponse(JSONObject json);
     }
 
-    public static void postRequestLogin(Context context, String email, String pw, JsonResponseHandler handler) {
+    public static void postRequestLogin(Context context, String email, String pw, final JsonResponseHandler handler) {
 
 //        안드로이드 앱이 클라이언트로써의 역할을 하돌고 도와주는 객체.
         OkHttpClient client = new OkHttpClient();
@@ -56,7 +57,23 @@ public class ServerUtil {
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
 
                 String body = response.body().string();
+
                 Log.d("서버연결성공", body);
+
+//                String body를 JSONObject 로 변환.
+
+                try {
+                    JSONObject jsonObject = new JSONObject(body);
+
+//                    변환된 JSON을 액티비티에 전달 + 처리 실행.
+                    if(handler != null) {
+                        handler.onResponse(jsonObject);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
 
