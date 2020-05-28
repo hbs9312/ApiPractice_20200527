a718ed36251bb02b0cbd9d26443c181e07276f07
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-28 10:01:37 +0900

    비밀번호 / 비밀번호 반복입력 확인 기능 - 다 맞춰놓고 비번만 바꾸면 => 달라짐을 인식 못함

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import org.w3c.dom.Text;
 
 import kr.co.tjoeun.apipractice_20200527.databinding.ActivitySignUpBinding;
 
@@ -14,7 +18,7 @@ public class SignUpActivity extends BaseActivity {
 //    비번을 타이핑할 때마다 길이 검사
 //     => 0글자 : 비밀번호를 입력해주세요.
 //     => 8글자 미만 : 비밀번호가 너무 짧습니다.
-//     => 그 이상 : 사용핻 좋은 비밀번호 입니다.
+//     => 그 이상 : 사용해도 좋은 비밀번호 입니다.
 
 //    비밀 확인도 타이핑할 때마다 검사.
 //     => 0글자 : 비밀번호 확인을 입력해주세요.
@@ -32,6 +36,63 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void setupEvent() {
+
+        binding.checkPasswordEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                String inputTxt = s.toString();
+
+                if(inputTxt.length() == 0) {
+                    binding.checkPasswordResultTxt.setText("비밀번호 확인을 입력해주세요.");
+                }
+                else if (!inputTxt.equals(binding.inputPasswordEdt.getText().toString())){
+                        binding.checkPasswordResultTxt.setText("비밀번호 재입력이  확인 되었습니다.");
+                }
+                else {
+                    binding.checkPasswordResultTxt.setText("비밀번호가 서로 다릅니다.");
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        binding.inputPasswordEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                String inputTxt = s.toString();
+
+                if(inputTxt.length() == 0) {
+                    binding.passwordResultTxt.setText("비밀번호를 입력해주세요.");
+                }
+                else if(inputTxt.length() < 8) {
+                    binding.passwordResultTxt.setText("비밀번호가 너무 짧습니다.");
+                }
+                else {
+                    binding.passwordResultTxt.setText("사용해도 좋은 비밀번호입니다.");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 
