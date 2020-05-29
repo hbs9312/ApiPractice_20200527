8d2eb0f675984c7d3a155fce4207d40e255965b1
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-29 15:36:54 +0900

    댓글 목록을 로그로 확인  - 리스트뷰로 뿌릴 준비

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/MainActivity.java
@@ -1,15 +1,19 @@
 package kr.co.tjoeun.apipractice_20200527;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,16 +38,32 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvent() {
 
+
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                ContextUtil.setLoginUserToken(mContext, "");
+                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                alert.setTitle("로그아웃");
+                alert.setMessage("정말 로그아웃 하시겠습니까?");
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ContextUtil.setLoginUserToken(mContext, "");
 
-                Intent myIntent = new Intent(mContext, LoginActivity.class);
-                startActivity(myIntent);
+                        Intent myIntent = new Intent(mContext, LoginActivity.class);
+                        startActivity(myIntent);
 
-                finish();
+                        finish();
+                    }
+                });
+                alert.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(mContext, "로그아웃을 취소했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.show();
 
             }
         });
@@ -68,6 +88,18 @@ public class MainActivity extends BaseActivity {
 
                         final JSONObject topic = data.getJSONObject("topic");
                         final Topic thisWeekTopic = Topic.getTopicFromJson(topic);
+
+//                        댓글목록도 파싱하자.
+
+                        JSONArray replies = data.getJSONArray("replies");
+
+                        for (int i=0; i < replies.length(); i++) {
+
+                            JSONObject reply = replies.getJSONObject(i);
+
+                            Log.d("댓글내용", reply.getString("content"));
+                        }
+
 
                         runOnUiThread(new Runnable() {
                             @Override
